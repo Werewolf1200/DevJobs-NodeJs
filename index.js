097b086fdf7ee9c43da709a24f3696bb9be85213
Saúl Camacho <electrohead1200@gmail.com>
2023-10-16 19:11:34 -0600

    Habilitando Handlebars como Template Engine, Creando Master Page y Página Principal

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
 const router = require('./routes');
 
 
 const app = express();
+
+// Habilitar Handlebars como View Engine
+app.engine('handlebars',
+    exphbs.engine({
+        defaultLayout: 'layout'
+    })
+);
+
+app.set('view engine', 'handlebars');
+
+// Static Files
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', router());
 
