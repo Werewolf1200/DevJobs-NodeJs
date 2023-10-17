59c9b4ae67e565b3e29418123dfe558be5d92a90
Saúl Camacho <electrohead1200@gmail.com>
2023-10-17 10:40:26 -0600

    Vacantes: Definiendo Modelo de Vacantes y Creando Formulario para Nuevas Vacantes

--- config/db.js
+++ config/db.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config({ path: 'variables.env' });
 
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+
 mongoose.connection.on('error', (error) => {
     console.log(error)
 })
+
+// Importar los Modelos
+require('../models/Vacantes');
